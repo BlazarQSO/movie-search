6bcccb2c090cd24b01cd7f6cb4981c059d848bbe
BlazarQSO <BlazarQSO@gmail.com>
2020-04-16 00:58:16 +0300

    feat: add keyboard

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style/style.scss';
+import './components/keyboard/keyboard.scss';
 import getRequest from './components/search/search';
 import Slider from './components/slider/slider';
+import buttons from './components/keyboard/buttons';
+import Keyboard from './components/keyboard/keyboard';
 
 function buttonClick(e, slider) {
     if (e.target.tagName === 'BUTTON') {
@@ -10,8 +13,40 @@ function buttonClick(e, slider) {
     }
 }
 
+function createInput(text) {
+    const input = document.createElement('input');
+    input.id = 'input';
+    input.className = 'search__wrap-input';
+    input.type = 'text';
+    input.placeholder = 'Search movie';
+    input.value = text;
+    document.getElementById('searchWrap').prepend(input);
+    input.focus();
+}
+
+let keyboard;
+function showKeyboard() {
+    const key = document.getElementById('key');
+    if (!key.classList.contains('click')) {
+        const input = document.getElementById('input');
+        input.classList.add('focus');
+        keyboard = new Keyboard(buttons, input);
+        keyboard.create('wrapKeyboard');
+        key.classList.add('click');
+    } else {
+        const text = document.getElementById('input').value;
+        document.getElementById('input').remove();
+        key.classList.remove('click');
+        createInput(text);
+        keyboard.destructorEvent();
+        keyboard = null;
+        document.getElementById('wrapKeyboard').innerHTML = '';
+    }
+}
+
 window.addEventListener('load', () => {
     document.getElementById('search').addEventListener('click', getRequest);
+    document.getElementById('key').addEventListener('click', showKeyboard);
 
     const rightBtn = document.getElementById('right');
     const leftBtn = document.getElementById('left');
@@ -20,4 +55,5 @@ window.addEventListener('load', () => {
     leftBtn.onclick = slider.moveLeft.bind(slider);
 
     document.getElementById('buttons').addEventListener('click', (e) => buttonClick(e, slider));
+    document.getElementById('input').focus();
 });
